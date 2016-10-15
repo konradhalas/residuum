d9137f60a34c5f0c17fab22804227507d9ca7bbe
konradhalas <halas.konrad@gmail.com>
2016-10-15 16:58:37 +0200

    Small changes intruduced during soldering

--- src/consts.h
+++ src/consts.h
@@ -1,6 +1,6 @@
-#define NEXT_BUTTON_PIN 23 // PF0
+#define NEXT_BUTTON_PIN 7 // PE6
 #define EDIT_BUTTON_PIN 13 // PC7
-#define PREVIOUS_BUTTON_PIN 7 // PE6
+#define PREVIOUS_BUTTON_PIN 23 // PF0
 
 #define LCD_LED_PIN 5 // PC6
 #define LCD_DC_PIN 8 // PB4
@@ -10,8 +10,8 @@
 #define RS_1_PIN 2  // PD1
 #define RS_2_PIN 0  // PD2
 #define RS_3_PIN 1  // PD3
-#define RS_4_PIN 4  // PD4
-#define RS_5_PIN 12 // PD6
+#define RS_4_PIN 12 // PD6
+#define RS_5_PIN 4  // PD4
 #define RS_6_PIN 18 // PF7
 #define RS_7_PIN 19  // PF6
 #define RS_8_PIN 20 // PF5
