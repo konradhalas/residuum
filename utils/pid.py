7029fad0c979a7572fec72d346fa504273f38091
konradhalas <halas.konrad@gmail.com>
2016-10-19 12:16:47 +0200

    Fixes

--- utils/pid.py
+++ utils/pid.py
@@ -1,3 +1,5 @@
+import argparse
+
 def line_range(sensors_count):
     return ((sensors_count * 1000 - 1000) / 2)
 
@@ -6,23 +8,25 @@ def pid(error, last_error, kp, kd):
 
 
 if __name__ == '__main__':
-    base_motors_speed = 120
-    sensors_count = 8
-    kp = 0.04
-    kd = 1
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--speed", type=int, required=True)
+    parser.add_argument("--sensors", type=int, required=True)
+    parser.add_argument("--kp", type=float, required=True)
+    parser.add_argument("--kd", type=int, required=True)
+    args = parser.parse_args()
 
-    for line_over in range(sensors_count):
-        error = line_over * 1000 - line_range(sensors_count)
+    for line_over in range(args.sensors):
+        error = line_over * 1000 - line_range(args.sensors)
         pid_delta = pid(
             error=error,
             last_error=error,
-            kp=kp,
-            kd=kd,
+            kp=args.kp,
+            kd=args.kd,
         )
         print("{} = error: {:5d}, delta: {:6.1f}, motors: {:3d}, {:3d}".format(
             line_over + 1,
             error,
             pid_delta,
-            base_motors_speed + pid_delta,
-            base_motors_speed - pid_delta,
+            args.speed + pid_delta,
+            args.speed - pid_delta,
         ))
